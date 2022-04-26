879456fb4fa2c31b092d7a0a5d83dcd947ddfab5
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-26 18:07:27 +0100

    getEmployerole tests and code to pass test written

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,12 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    let x = []
+
+    employees.forEach(function(o){
+        if (o.name === employeeName) x.push(o.role);} );
+
+        return x.toString()
+    };
+
+
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,13 @@
+/* eslint-disable prettier/prettier */
 const { getEmployerRole } = require("../src");
 
-describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
-});
+  describe("getEmployerRole", () => {
+    test("returns the employee's role in the company", () => {
+      const employees = [{name: "Satti", role: "Developer"}, {name: "Jenny", role: "Sales Associate"},
+    {name: "Javid", role: "Human Resources"}];
+    
+    expect(getEmployerRole("Javid", employees)).toBe("Human Resources");
+    expect(getEmployerRole("Satti", employees)).toBe("Developer");
+    expect(getEmployerRole("Jenny", employees)).toBe("Sales Associate");
+    });
+  });
