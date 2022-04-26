7bcb1342d48932aa2fdc8344cb66eca0c59c05fa
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-26 11:57:33 +0100

    numberToReverseDigits & human CatDogYears completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,15 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    if (number === 1) {
+        return [1,15,15]
+    } if (number === 2) {
+        return [2,24,24]
+    } if (number > 2) {
+        return [number, 24 + ((number -2) * 4), 24 + ((number -2) * 5)]
+    } if (number === 0) {
+        return [0, 0, 0]
+    } if (number < 0) {
+        return "Error, humans, cats and dogs can't have an age of less than 0"
+    }
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,12 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    test("returns an array showing the age in human years, cat years and dog years", () => {
+      expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+      expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+      expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+      expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+      expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+      expect(humanCatDogYears(-5)).toEqual("Error, humans, cats and dogs can't have an age of less than 0");
+    });
+  });
