58dfb09526ab49eaa017ab1a6287b3b543551407
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-26 10:43:55 +0100

    booleanToWord completed

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,10 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits([5,4,3,2,1])).toEqual([1,2,3,4,5]);
+    expect(numberToReversedDigits([4,5,7,3,2])).toEqual([2,3,7,5,4]);
+    expect(numberToReversedDigits([15,4,23,2,14])).toEqual([14,2,23,4,15]);
+    expect(numberToReversedDigits([50,400,3,87,16])).toEqual([16,87,3,400,50]);
+  });
 });
