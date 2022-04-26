b3dab8aee4ac76d052f160b0d45638f62598dc7f
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-26 14:33:26 +0100

    joinNames test written and code to pass test written

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+   const names = namesObj.map(obj => obj.name);
+   const finalName = names.pop();
+
+   return names.length ? names.join(', ') + ' & ' + finalName : finalName;
+};
 
 module.exports = joinNames;
