e5501bc429ea021dd2e54a7048fa5a11b19a9f1a
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-26 12:58:41 +0100

    Reach destination test written and code to pass test written

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,11 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const duration = distance / speed 
+    const durationRounded = Math.round(duration*2)/2
+
+    if (isNaN(distance) || isNaN(speed)) {
+        return "Error. Both distance and speed need to be numbers to calculation the journey duration."
+    }; 
+    return `I should be there in ${durationRounded} hours.`
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,12 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(50, 20)).toBe("I should be there in 2.5 hours.");
+    expect(reachDestination(75, 96)).toBe("I should be there in 1 hours.");
+    expect(reachDestination("Hello", 96)).toBe("Error. Both distance and speed need to be numbers to calculation the journey duration.");
+    expect(reachDestination(10, "Hello")).toBe("Error. Both distance and speed need to be numbers to calculation the journey duration.");
+    expect(reachDestination("Hello", "Timmy")).toBe("Error. Both distance and speed need to be numbers to calculation the journey duration.");
+  });
 });
